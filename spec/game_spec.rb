594f5489d317b06fccbb5c0d871d5f0eb1527565
Xolani Mfuko <xmfuko@gmail.com>
2014-08-10 17:15:24 -0700

    move deck to the initialize method, instead of its own method

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -10,4 +10,14 @@ describe Game do
     go_fish = Game.new({:name => "Go Fish"})
     expect(go_fish.name).to eq "Go Fish"
   end
+
+  it "initializes all the cards in a deck and returns the length of 52 for the deck to peove its existence" do
+    go_fish = Game.new({:name => "Go Fish"})
+    expect(go_fish.deck.length).to eq 52
+  end
+
+  it "returns true if there are still cards in the deck, false if the deck is empty" do
+    go_fish = Game.new({:name => "Go Fish"})
+    expect(go_fish.deck_in_play?).to eq true
+  end
 end
