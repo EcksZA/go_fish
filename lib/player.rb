80579590ba231c2588cca64dec68e644a818076a
Xolani Mfuko <xmfuko@gmail.com>
2014-08-12 19:58:27 -0700

    create a method that adds a score for a player when the player collects a set of cards.

--- lib/player.rb
+++ lib/player.rb
@@ -1,14 +1,34 @@
+require 'pry'
+
 class Player
-  attr_accessor :name, :current_deck
+  attr_accessor :name, :current_deck, :score
 
   def initialize(hash)
     @name = hash[:name]
     @current_deck = []
+    @score = 0
   end
 
   def add_card(new_card)
       moved_card = new_card
       @current_deck << moved_card
+  end
+
+  def score
+    index = 0
+    test_card = []
+    @current_deck.each {|card| test_card << card.chop}
+
+    test_card.each do |card|
+      if test_card.count(card) == 4
+        @current_deck.delete_if {|cut| cut[0] == card}
+        test_card.delete(card)
+        @score += 1
+        index -= 1
+      end
+      index += 1
+    end
+    @score
   end
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -19,4 +19,14 @@ describe Player do
     expect(player_1.current_deck.length).to eq 1
   end
 
+  it "checks if a player has a complete set and returns a score for the player" do
+    go_fish = Game.new({:name => "Go Fish"})
+    player_1 = Player.new({:name => "Xolani"})
+    player_1.current_deck << "AS"
+    player_1.current_deck << "AD"
+    player_1.current_deck << "AC"
+    player_1.current_deck << "AH"
+    player_1.current_deck << "2H"
+    expect(player_1.score).to eq 1
+  end
 end
